3ba712294a8f41daff0d053856876c3cf5b6e0c2
Serhii Plis <serzh2577@gmail.com>
2023-11-04 13:48:12 +0300

    start

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const listCategoriesEl = document.querySelector('#categories');
+
+console.log(listCategoriesEl);
+
+// function countsItemsInCategoryList() {
+//   let total = 0;
+//   [...listCategoriesEl].forEach(element => {
+//     total += 1;
+//   });
+//   return `Количество категорий ${total}`;
+// }
+
+// console.log(countsItemsInCategoryList());
