3311c90ccc3aa986d05aa70e25b287d1245c3d94
Serhii Plis <serzh2577@gmail.com>
2023-11-07 01:19:42 +0300

    task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
+const inputEl = document.querySelectorAll('.login-form input');
 
+formEl.addEventListener('submit', getsUserLoginDetails);
+
+function getsUserLoginDetails(evt) {
+  evt.preventDefault();
+
+  const emailValue = evt.target.elements.email.value;
+  const passwordValue = evt.target.elements.password.value;
+
+  if (emailValue.trim() && passwordValue.trim()) {
+    const obj = {};
+
+    inputEl.forEach(el => {
+      obj[el.getAttribute('name')] = el.value.trim();
+    });
+
+    formEl.reset();
+
+    return console.log(obj);
+  }
+
+  alert('All form fields must be filled in');
+}
