f385bf4ff3db4544c3723df7bbe267fdaa813eee
Serhii Plis <serzh2577@gmail.com>
2023-11-08 20:44:52 +0300

    task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,37 @@
+const inputEl = document.querySelector('#controls input');
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDestroyEl = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+btnCreateEl.addEventListener('click', onOoo);
+btnDestroyEl.addEventListener('click', onXxx);
+
+function onOoo() {
+  if (inputEl.value === '' || inputEl.value < 1 || inputEl.value > 100) {
+    return;
+  }
+
+  boxesEl.innerHTML = '';
+  let size = 20;
+
+  for (let i = 0; i < inputEl.value; i++) {
+    size += 10;
+    const tagDiv = document.createElement('div');
+    tagDiv.style.marginBottom = '5px';
+    tagDiv.style.width = size + 'px';
+    tagDiv.style.height = size + 'px';
+    tagDiv.style.backgroundColor = getRandomHexColor();
+    boxesEl.appendChild(tagDiv);
+  }
+
+  inputEl.value = '';
+}
+
+function onXxx() {
+  boxesEl.innerHTML = '';
+  inputEl.value = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
